229fc46e5c24f9d372fc2e498af42105f0be80f8
Athina Lange <alange@ucsd.edu>
2021-07-21 10:18:53 -0700

    Update rotate_shorenormal.m

--- rotate_shorenormal.m
+++ rotate_shorenormal.m
@@ -1,29 +1,33 @@
 %% Rotate to shorenormal coordinates
 %
-% From a buoy coordinate system (WNU), pulling shorenormal angle from CDIP,
+% From ENU coordinate system, pulling shorenormal angle from CDIP,
 % rotating to onshore, shorenormal (+x) and alongshore, north (+y) [Right-handed coordinate system]
 %
 % Input: MOP: double
 %        U,V: velocities in buoy coordinates [1 x time] 
 %
-% Author: 
+%% Author: 
 % Athina Lange, SIO July 2021
-%
 
-function [Uprime, Vprime, shorenormal] = rotate_shorenormal(MOP, U, V);
 
-url = ['https://cdip.ucsd.edu/mops/?moplist=San_Diego_County&pub=public&mop=D0' char(string(MOP)) '&xitem=info']
-name = 'Normal';
-shorenormal = urlfilter(url,name)
+function [Uprime, Vprime, shorenormal] = rotate_shorenormal(U, V);
 
-rotation = shorenormal - 90; % +x is Onshore, Shorenormal, +y is South, alongshore
+load('moplist') % fulll list of all CDIP buoy's as of 07/22/19
+[indx,tf] = listdlg('ListString',moplist, 'SelectionMode','single', 'InitialValue',[1], 'Name', 'What MOP do you want to use?');
+stn = char(moplist(indx));
+ncfile = strcat('http://thredds.cdip.ucsd.edu/thredds/dodsC/cdip/model/MOP_alongshore/', string(stn(end-4:end)), '_nowcast.nc');
+shorenormal=ncread(ncfile,'metaShoreNormal'); % mop shore normal
+
+% Coordinate system is in ENU, so rotate +x to North, then CCW rotation to
+% MOP shorenormal, then rotate 180 to get 'going to' frame of reference. 
+rotation = rem(90+(360 - shorenormal)+180, 360); % +x is Onshore, Shorenormal, +y is North, alongshore
 alpha_rad = rotation*pi/180; 
-rot_mat   = [cos(alpha_rad)  sin(alpha_rad);
-            -sin(alpha_rad)  cos(alpha_rad)];
+rot_mat   = [cos(alpha_rad)  -sin(alpha_rad);
+             sin(alpha_rad)  cos(alpha_rad)];
 
 uv=rot_mat*[U';V'];
 Uprime=uv(1,:).'; 
-Vprime=-uv(2,:).';
+Vprime=uv(2,:).';
 
 
 end
