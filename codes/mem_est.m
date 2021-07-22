38521df17a5c91f31c5e166d69f0b0c06e6e049b
Athina Lange <alange@ucsd.edu>
2021-07-22 11:16:50 -0700

    Update and rename mem_est.m to codes/mem_est.m

--- codes/mem_est.m
+++ codes/mem_est.m
@@ -1,27 +1,5 @@
 function [mem, x, y] = mem_est(a1,a2,b1,b2)
-%     c1 = complex(a1,b1);
-%     c2 = complex(a2,b2);
-% 
-%     p1=(c1-c2.*conj(c1))./(1-abs(c1).^2);
-%     p2=c2-c1.*p1;
-% 
-%     x=1.-p1.*conj(c1)-p2.*conj(c2);
-%     
-%     a=1:360;
-%     a=a.*pi/180;
-%     e1=complex(1.,0)*cos(a)-complex(0,1.)*sin(a);
-%     e2=complex(1.,0)*cos(2*a)-complex(0,1.)*sin(2*a);
-%     y=abs(complex(1.,0)-p1*e1-p2*e2).^2;
-%     
-%     mem=abs((x'*ones(1,360))./y);
-% 
-%     % normalize MEM distribution
-%     mem=((1./sum(mem'))'*ones(1,360)).*mem;
-%     
-%     %  direction indices are reveresed after matrix
-%     %  calcs, so flip directions back to native 
-%     %  direction coordinates
-%     mem=fliplr(mem);
+%% Bill's code
 
 d1=a1;d2=b1;d3=a2;d4=b2;
 
