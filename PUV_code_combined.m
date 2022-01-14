d238d7f669fb78f9ab04424198ddba1966adb0bd
Athina Lange <alange@ucsd.edu>
2022-01-13 16:25:57 -0800

    Update PUV_code_combined.m

--- PUV_code_combined.m
+++ PUV_code_combined.m
@@ -2,100 +2,81 @@
 % 
 % %% Initial QC
 % % PUV_1QC
-% load Torrey_1920_582_10_processed.mat
-% %% Rotate to shorenormal coordinates
-% [PUV.ShorenormalCoord.U, PUV.ShorenormalCoord.V, shorenormal] = rotate_shorenormal(PUV.BuoyCoord.U, PUV.BuoyCoord.V);
-% 
-% %%
-% tic
-% % 66cm sand to top of pressure port on deployment. 78cm on recovery. 
-% doffp = 0.72; % splitting the difference between the two
-%     doffu = doffp+0.371; %adding 37.1 cm to u,v sample point
-% 
-%     PUV_work = PUV;
-% 
-% %% Interp NaNs if less than 2sec gap
-% PUV_work.P = fillmissing(PUV.P,'linear','SamplePoints',PUV.time,'MaxGap',seconds(2));
-% PUV_work.BuoyCoord.U = fillmissing(PUV.BuoyCoord.U,'linear','SamplePoints',PUV.time,'MaxGap',seconds(2));
-% PUV_work.BuoyCoord.V = fillmissing(PUV.BuoyCoord.V,'linear','SamplePoints',PUV.time,'MaxGap',seconds(2));
-% PUV_work.BuoyCoord.W = fillmissing(PUV.BuoyCoord.W,'linear','SamplePoints',PUV.time,'MaxGap',seconds(2));
-% PUV_work.ShorenormalCoord.U = fillmissing(PUV.ShorenormalCoord.U,'linear','SamplePoints',PUV.time,'MaxGap',seconds(2));
-% PUV_work.ShorenormalCoord.V = fillmissing(PUV.ShorenormalCoord.V,'linear','SamplePoints',PUV.time,'MaxGap',seconds(2));
-% PUV_work.T = fillmissing(PUV.T,'linear','SamplePoints',PUV.time,'MaxGap',seconds(2));
-% toc
-% %% Split data into hour segments
-% hourlen = (60*60*PUV_work.fs)
-% segtotal = floor(length(PUV_work.time)/hourlen)
-% PUV_work.time(segtotal*hourlen+1:end)=[];
-% PUV_work.P(segtotal*hourlen+1:end)=[];
-% PUV_work.BuoyCoord.U(segtotal*hourlen+1:end)=[];
-% PUV_work.BuoyCoord.V(segtotal*hourlen+1:end)=[];
-% PUV_work.BuoyCoord.W(segtotal*hourlen+1:end)=[];
-% PUV_work.ShorenormalCoord.U(segtotal*hourlen+1:end)=[];
-% PUV_work.ShorenormalCoord.V(segtotal*hourlen+1:end)=[];
-% 
-% PUV_work.time = reshape(PUV_work.time, hourlen, segtotal);
-% PUV_work.P = reshape(PUV_work.P, hourlen, segtotal);
-% PUV_work.BuoyCoord.U = reshape(PUV_work.BuoyCoord.U, hourlen, segtotal);
-% PUV_work.BuoyCoord.V = reshape(PUV_work.BuoyCoord.V, hourlen, segtotal);
-% PUV_work.BuoyCoord.W = reshape(PUV_work.BuoyCoord.W, hourlen, segtotal);
-% 
-% PUV_work.ShorenormalCoord.U = reshape(PUV_work.ShorenormalCoord.U, hourlen, segtotal);
-% PUV_work.ShorenormalCoord.V = reshape(PUV_work.ShorenormalCoord.V, hourlen, segtotal);
-% 
-% P = PUV_work.P;
-% U = PUV_work.BuoyCoord.U;
-% V = PUV_work.BuoyCoord.V;
-% W = PUV_work.BuoyCoord.W;
-% fs = PUV_work.fs;
-% time = PUV_work.time;
-% toc
-% %% Get tides to check
-% disp('Getting tides for a sanity check')
-% datum = 'NAVD';
-% station =  '9410230'; % La Jolla
-% [tide_t_verified,tide_t_predicted,tide_z_verified,tide_z_predicted,tideInfo] = getNOAAtide(time(1,1),time(end,end),station,datum);
-% % A1=split(importdata('Nov.txt', ' '));
-% % A2=split(importdata('Dec.txt', ' '));
-% % A3=split(importdata('Jan.txt', ' '));
-% % A4=split(importdata('Feb.txt', ' '));
-% % A5=split(importdata('Mar.txt', ' '));
-% % 
-% % tide_time = [datetime(A1(2:end,1), 'InputFormat', 'yyyy/MM/dd') + timeofday(datetime(A1(2:end,3), 'InputFormat', 'HH:mm'));...
-% % 	datetime(A2(2:end,1), 'InputFormat', 'yyyy/MM/dd') + timeofday(datetime(A2(2:end,3), 'InputFormat', 'HH:mm'));...
-% % 	datetime(A3(2:end,1), 'InputFormat', 'yyyy/MM/dd') + timeofday(datetime(A3(2:end,3), 'InputFormat', 'HH:mm'));...
-% % 	datetime(A4(2:end,1), 'InputFormat', 'yyyy/MM/dd') + timeofday(datetime(A4(2:end,3), 'InputFormat', 'HH:mm'));...
-% % 	datetime(A5(2:end,1), 'InputFormat', 'yyyy/MM/dd') + timeofday(datetime(A5(2:end,3), 'InputFormat', 'HH:mm'))];
-% % 
-% % tide = [str2num(char(A1(2:end,4)));...
-% %     str2num(char(A2(2:end,4)));...
-% %     str2num(char(A3(2:end,4)));...
-% %     str2num(char(A4(2:end,4)));...
-% %     str2num(char(A5(2:end,4)))];
-% 
-% %% Get useable values
-% disp('Getting wave stats, writing to PUV_process_ENU')
-% tic
-% Uprime = PUV_work.BuoyCoord.U;
-% Vprime = PUV_work.BuoyCoord.V;
-% for ii = 1:segtotal
-%     if isempty(find(isnan(P(:,ii))))
-%         [PUV_process_ENU(ii)] = vector_wave_stats_mspec(Uprime(:,ii), Vprime(:,ii), P(:,ii), doffp, doffu);
-%     end
-% end
-% toc 
-% %%
-% disp('Getting wave stats, writing to PUV_process')
-% tic
-% Uprime = PUV_work.ShorenormalCoord.U;
-% Vprime = PUV_work.ShorenormalCoord.V;
-% for ii = 1866:segtotal
-%     if isempty(find(isnan(P(:,ii))))
-%         [PUV_process(ii)] = vector_wave_stats_mspec(Uprime(:,ii), Vprime(:,ii), P(:,ii), doffp, doffu);
-%     end
-% end
-% toc
+addpath(genpath('/Users/athinalange/Desktop/IG_BC_remote_work/PUV_Processing-julia/codes'))
+addpath(genpath('/Users/athinalange/Desktop/IG_BC_remote_work/PUV_Processing-julia/boundwave'))
+addpath(genpath('/Users/athinalange/Desktop/IG_BC_remote_work/PUV_Processing-julia/1stQC'))
+load Torrey_1920_582_10_processed.mat
+%% Rotate to shorenormal coordinates
+[PUV.ShorenormalCoord.U, PUV.ShorenormalCoord.V, shorenormal] = rotate_shorenormal(PUV.BuoyCoord.U, PUV.BuoyCoord.V);
 
+%%
+tic
+% 66cm sand to top of pressure port on deployment. 78cm on recovery. 
+doffp = 0.72; % splitting the difference between the two
+    doffu = doffp+0.371; %adding 37.1 cm to u,v sample point
+
+    PUV_work = PUV;
+
+%% Interp NaNs if less than 2sec gap
+PUV_work.P = fillmissing(PUV.P,'linear','SamplePoints',PUV.time,'MaxGap',seconds(2));
+PUV_work.BuoyCoord.U = fillmissing(PUV.BuoyCoord.U,'linear','SamplePoints',PUV.time,'MaxGap',seconds(2));
+PUV_work.BuoyCoord.V = fillmissing(PUV.BuoyCoord.V,'linear','SamplePoints',PUV.time,'MaxGap',seconds(2));
+PUV_work.BuoyCoord.W = fillmissing(PUV.BuoyCoord.W,'linear','SamplePoints',PUV.time,'MaxGap',seconds(2));
+PUV_work.ShorenormalCoord.U = fillmissing(PUV.ShorenormalCoord.U,'linear','SamplePoints',PUV.time,'MaxGap',seconds(2));
+PUV_work.ShorenormalCoord.V = fillmissing(PUV.ShorenormalCoord.V,'linear','SamplePoints',PUV.time,'MaxGap',seconds(2));
+PUV_work.T = fillmissing(PUV.T,'linear','SamplePoints',PUV.time,'MaxGap',seconds(2));
+toc
+%% Split data into hour segments
+hourlen = (60*60*PUV_work.fs)
+segtotal = floor(length(PUV_work.time)/hourlen)
+PUV_work.time(segtotal*hourlen+1:end)=[];
+PUV_work.P(segtotal*hourlen+1:end)=[];
+PUV_work.BuoyCoord.U(segtotal*hourlen+1:end)=[];
+PUV_work.BuoyCoord.V(segtotal*hourlen+1:end)=[];
+PUV_work.BuoyCoord.W(segtotal*hourlen+1:end)=[];
+PUV_work.ShorenormalCoord.U(segtotal*hourlen+1:end)=[];
+PUV_work.ShorenormalCoord.V(segtotal*hourlen+1:end)=[];
+
+PUV_work.time = reshape(PUV_work.time, hourlen, segtotal);
+PUV_work.P = reshape(PUV_work.P, hourlen, segtotal);
+PUV_work.BuoyCoord.U = reshape(PUV_work.BuoyCoord.U, hourlen, segtotal);
+PUV_work.BuoyCoord.V = reshape(PUV_work.BuoyCoord.V, hourlen, segtotal);
+PUV_work.BuoyCoord.W = reshape(PUV_work.BuoyCoord.W, hourlen, segtotal);
+
+PUV_work.ShorenormalCoord.U = reshape(PUV_work.ShorenormalCoord.U, hourlen, segtotal);
+PUV_work.ShorenormalCoord.V = reshape(PUV_work.ShorenormalCoord.V, hourlen, segtotal);
+
+save('PUV_work','PUV_work', '-v7.3')
+toc
+%%
+disp('Getting wave stats, writing to PUV_process')
+tic
+P = PUV_work.P;
+U = PUV_work.BuoyCoord.U;
+V = PUV_work.BuoyCoord.V;
+W = PUV_work.BuoyCoord.W;
+fs = PUV_work.fs;
+time = PUV_work.time;
+toc
+
+
+Uprime = PUV_work.ShorenormalCoord.U;
+Vprime = PUV_work.ShorenormalCoord.V;
+for ii = 1:size(P,2)
+    if isempty(find(isnan(P(:,ii))))
+        [etaAa, etaAss, etaAig, tm]= etasolver(P(:,ii), time(:,ii), size(P,1));
+        eta_level(ii).P = P(:,ii);
+        eta_level(ii).etaAa = etaAa;
+        eta_level(ii).etaAss = etaAss;
+        eta_level(ii).etaAig = etaAig;
+        eta_level(ii).time = time;
+        [eta_level(ii).m2,eta_level(ii).m3] = sectormoments(7200/2, eta_level(ii).P , [0.004 0.04 0.25],1);
+        [PUV_process(ii)] = vector_wave_stats_mspec(Uprime(:,ii), Vprime(:,ii), P(:,ii), doffp, doffu);
+    end
+end
+toc
+%save('eta', 'eta_level', '-v7.3')
+save('PUV_process.mat', 'PUV_process')
 %% load variables up to this point
 load temp
 
@@ -458,7 +439,7 @@ hline(0.8)
 set(gcf, 'Position', [100,100,1000,600])
 saveas(gcf,'Ztest.png')
 %% Mean direction (f)
-id = 112; disp(['Selected comparison time is ' datestr(PUV_work.time(1,id)) ' , Hs = ' num2str(Hsig.Hs(id),'%2.2f') 'm'])
+id = 12%112; %disp(['Selected comparison time is ' datestr(PUV_work.time(1,id)) ' , Hs = ' num2str(Hsig.Hs(id),'%2.2f') 'm'])
 
 figure(2);clf
 subplot(121)
@@ -481,7 +462,7 @@ title('SS')
 sgtitle([string(time(1,id)) '- SN Coordinates'])
 
 set(gcf, 'Position', [100,100,1000,600])
-saveas(gcf,['Dir_polar_id' num2str(id) '_SN_CCW.png'])
+%saveas(gcf,['Dir_polar_id' num2str(id) '_SN_CCW.png'])
 %% Reflection Coeff
 figure(3);clf
 subplot(311)
